edb43b6638a842e216fb9b48d0e71a6d8a9b50ad
Anti Admin <robot@antigate.com>
2021-12-08 11:57:01 +0300

    pushAntiGateVariable method and example

--- anticaptchaofficial/antigatetask.py
+++ anticaptchaofficial/antigatetask.py
@@ -7,6 +7,42 @@ class antigateTask(antiNetworking):
     template_name = ""
     variables = {}
 
+    def push_variable(self, task_id, name, value):
+        result = self.make_request("pushAntiGateVariable", {
+            "clientKey": self.client_key,
+            "taskId": self.task_id,
+            "name": name,
+            "value": value
+        })
+        if result == 0:
+            return 0
+        else:
+            if result["errorId"] == 0:
+                return 1
+            else:
+                return 0
+
+    def send_antigate_task(self):
+        if self.create_task({
+            "clientKey": self.client_key,
+            "task": {
+                "type": "AntiGateTask",
+                "websiteURL": self.website_url,
+                "templateName": self.template_name,
+                "variables": self.variables,
+                "proxyAddress": self.proxy_address,
+                "proxyPort": self.proxy_port,
+                "proxyLogin": self.proxy_login,
+                "proxyPassword": self.proxy_password
+            }
+        }) == 1:
+            self.log("created task with id "+str(self.task_id))
+            return self.task_id
+        else:
+            self.log("could not create task")
+            self.log(self.err_string)
+            return 0
+
     def solve_and_return_solution(self):
         if self.create_task({
             "clientKey": self.client_key,
--- antigate_2fa_delay_example.py
+++ antigate_2fa_delay_example.py
@@ -1,19 +1,30 @@
 from anticaptchaofficial.antigatetask import *
+import time
 
 solver = antigateTask()
 solver.set_verbose(1)
 solver.set_key("YOUR_KEY")
-solver.set_website_url("http://antigate.com/logintest.php")
-solver.set_template_name("Sign-in and wait for control text")
+solver.set_website_url("http://antigate.com/logintest2fa.php")
+solver.set_template_name("Sign-in with 2FA and wait for control text")
 solver.set_variables({
     "login_input_css": "#login",
     "login_input_value": "test login",
     "password_input_css": "#password",
     "password_input_value": "test password",
+    "2fa_input_css": "#2facode",
+    "2fa_input_value": "_WAIT_FOR_IT_",
     "control_text": "You have been logged successfully"
 })
 
-result  = solver.solve_and_return_solution()
+task_id  = solver.send_antigate_task()
+if task_id == 0:
+    print("could not create task")
+    exit()
+
+time.sleep(5)  # emulating actual 2fa retrieval
+solver.push_variable(task_id, "2fa_input_value", "349001")
+result = solver.wait_for_result(600)
+
 if result != 0:
     cookies, localStorage, fingerprint, url, domain = result["cookies"], result["localStorage"], result["fingerprint"], result["url"], result["domain"]
     print("cookies: ", cookies)
